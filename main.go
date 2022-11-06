b611117ed668c9ca2724791e5bce9f67649db34b
huazhongchan <951319124@qq.com>
2022-11-06 23:29:23 +0800

    render index.html

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, zw")
+		content, err := os.ReadFile("./website/index.html")
+		if err != nil {
+			c.String(404, "?")
+		} else {
+			c.Data(200, "text/html; charset=utf-8", content)
+		}
 	})
 	r.Run(":80") // listen and serve on 0.0.0.0:8080
 }
